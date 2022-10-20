93544b515fea34dd2d13ab6d788c60f292bc5af8
Zeelgabriels <zealgabrles@gmail.com>
2022-10-19 17:47:42 -0700

    Task_4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for the char alphabet
+ * _isalpha - checks for char alphabet
  * @c: the char to be checked
  *
  * Return: 1 if char is letter lower or upper, 0 otherwise
