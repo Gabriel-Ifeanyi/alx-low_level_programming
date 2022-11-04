588dfaa749ed9aa436516d79aa649a45263b5c67
Zeelgabriels <zealgabrles@gmail.com>
2022-11-03 20:23:33 -0700

    Task_5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,13 +17,13 @@ int sqrt2(int a, int b)
 	return (sqrt2(a, b + 1));
 }
 /**
- * _sprt_recursion - Would return the natural square root of a number n.
+ * _sprt_recursion - Would return the natural square root of a number n
  * @n: number integer
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _sqrt_recursion(int n)
+int _sqrt_recursion(int n);
 {
 	return (sqrt2(n, 1));
 }
